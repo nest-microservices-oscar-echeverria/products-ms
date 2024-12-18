2e4ff1b7c6c6f36da0dce61714bf6d23a34c8e8c
oskrecheverria1996 <oskr.echeverria1996@gmail.com>
2024-12-18 11:57:29 -0500

    @main - change to nats transport

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { envs } from './config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-
+  
   const logger = new Logger('Main')
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port
+        servers: envs.natsServers
       }
     }
   );
